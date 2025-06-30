8ace0851c2db8f1dc84344aad3a9c849f2fb0976
Sangram Badi <sangram.bachu@gmail.com>
2025-06-30 22:56:02 +0530

    Todo App

--- class-3/todo.py
+++ class-3/todo.py
@@ -1,24 +1,23 @@
 import streamlit as st
 
-input = st.text_input(label="Todo")
-
-
+todoList = []
 if 'todoList' not in st.session_state:
     st.session_state.todoList = []
+
+input = st.text_input(label="Todo")
 
 if st.button("Save"):
     st.session_state.todoList.append(input)
 
-
-# Render todoList
 if len(st.session_state.todoList) > 0:
     for index, todo in enumerate(st.session_state.todoList):
         col1, col2 = st.columns([8, 2])
         with col1:
             st.write(f"{todo}")
         with col2:
-            if st.button(label='❌', key=f"del_{index}"):
+            if st.button(label='❌', key=f"dtn_{index}"):
                 st.session_state.todoList.pop(index)
                 st.rerun()
+
 else:
-    st.info("Bhai !! Saab Kaam Khatam ")
+    st.info("Bahi!!! Saab Kaam Khatam")
